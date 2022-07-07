aefb969aba7b628d388106aefae49c560d2fe435
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-07 01:43:46 -0300

    docs: swagger initial config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const databaseConnection = require('./database/dbConnection');
 const characterRoutes = require('./routes/characters.route');
+const swaggerRoutes = require('./routes/swagger.routes');
 
 require('dotenv').config();
 
@@ -14,7 +15,9 @@ databaseConnection();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/characters', characterRoutes);
+app.use('/api', swaggerRoutes);
 
 app.listen(port, () => {
   console.log(`server running @ http://localhost:${port} 🔗`);
