0be9208f7f875eeedefda9c712e990807f839e64
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-07 00:23:04 -0300

    chore: Character Model

--- src/characters/Character.js
+++ src/characters/Character.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const CharacterSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true, unique: true },
+  },
+  { versionKey: false },
+);
+
+const Character = mongoose.model('Character', CharacterSchema, 'characters');
+
+module.exports = Character;
