cfa32c48e154e9bcf25ddfc387004823b580b7c0
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-07 01:20:58 -0300

    style: silly fixes

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -2,14 +2,12 @@
 const mongoose = require('mongoose');
 
 const databaseConnection = () => {
-  console.log('connecting to database');
-
   mongoose
     .connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('MONGO DB CONNECTED ❤'))
+    .then(() => console.log('mongodb connected ❤'))
     .catch((err) => console.log(`error connecting to database: ${err}`));
 };
 
