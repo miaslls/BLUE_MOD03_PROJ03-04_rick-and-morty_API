05f3608465bb673680afb3c82d7d6be9e0ff02b2
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-09 01:13:10 -0300

    docs: swagger minor fixes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/characters', characterRoutes);
-app.use('/api', swaggerRoutes);
+app.use('/api-docs', swaggerRoutes);
 
 app.listen(port, () => {
   console.log(`server running @ port ${port}`);
--- src/swagger/swagger.routes.js
+++ src/swagger/swagger.routes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
