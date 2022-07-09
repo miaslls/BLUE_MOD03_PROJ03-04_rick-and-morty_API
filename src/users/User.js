fc140810e7c064b1970f5b854b6813e1805f0d35
miaslls <97785569+miaslls@users.noreply.github.com>
2022-07-09 02:43:39 -0300

    chore: User model

--- src/users/User.js
+++ src/users/User.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, select: false },
+    photo: { type: String, required: true },
+  },
+  { versionKey: false },
+);
+
+const User = mongoose.model('User', UserSchema, 'users');
+
+module.exports = User;
